36deb47902526c0c8a0b443220c9f17e003eced9
julienPalleau <jpalleau@gmail.com>
2023-11-06 21:42:14 +0100

    added chapter query parameters

--- NeuraNine/1-RequestsLibraryInPython-BeginnerCrashCourse.py
+++ NeuraNine/1-RequestsLibraryInPython-BeginnerCrashCourse.py
@@ -8,13 +8,28 @@ url = "https://httpbin.org/get?name=Mike&age=25"
 response = requests.get(url)
 res_json = response.json()
 del res_json['origin']
-print(res_json)
+print(f'result of requests.get in json format response = requests.get("https://httpbin.org/get?name=Mike&age=25"): \n{res_json}')
 
 response = requests.get(url, params=params)
-print(response.url)
+print("\n")
+print(f"url: \n{response.url}")
 res_json = response.json()
 del res_json['origin']
-print(res_json)
+print("\n")
+print(f"result of requests.get in json format but with a different request format response = requests.get(url, params=params): \n{res_json}")
+
+payload = {
+    "name": "Mike",
+    "age": 25
+}
+# The post request
+response = requests.post("https://httpbin.org/post", data=payload)
+print("\n")
+print(f"url: \n{response.url}")
+res_json = response.json()
+del res_json['origin']
+print("\n")
+print(f'result of requests.post in json format check the code response = requests.post("https://httpbin.org/post", data=payload): \n{res_json}')
 
 # the error codes
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
@@ -28,10 +43,10 @@ else:
 
 response = requests.get("https://httpbin.org/status/401")
 # In the example below we catch any error
-print(response.raise_for_status())
+# print(response.raise_for_status())
 
 
 # User agent
-import requests
 response = requests.get("https://httbin.org/user-agent")
+print("\n")
 print(response.text)
