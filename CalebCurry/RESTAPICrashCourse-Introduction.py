d1c681c111cb0c3445ca109bd0f9279f95e8d7a9
julienPalleau <jpalleau@gmail.com>
2023-12-11 15:18:17 +0100

    re-sync

--- CalebCurry/RESTAPICrashCourse-Introduction.py
+++ CalebCurry/RESTAPICrashCourse-Introduction.py
@@ -1,1 +1,15 @@
 # https://www.youtube.com/watch?v=qbLc5a9jdXo
+import requests
+import json
+
+response = requests.get('http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print("skipped")
+    print()
+
+
